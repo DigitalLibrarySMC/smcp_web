a4ae4d14c2ef45aa84853bdc4e72f27c59a3c952
AbelLouis7 <abellouisfernandez@gmail.com>
2023-10-21 08:26:10 +0530

    button design

--- backend/static/js/home.js
+++ backend/static/js/home.js
@@ -12,3 +12,15 @@ redirectButtons.forEach(function(button) {
         window.location.href = targetUrl;
     });
 });
+
+var redirectButtons = Array.from(document.getElementsByClassName("log-button"));
+redirectButtons.forEach(function(button) {
+    button.addEventListener("click", function () {
+        // Specify the URL you want to redirect to for each button
+        var targetUrl = button.getAttribute("data-href");
+        console.log('targetUrl')
+
+        // Perform the redirection
+        window.location.href = targetUrl;
+    });
+});
